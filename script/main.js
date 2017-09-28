da281aea7a32af428137217eca140487e8b98b48
Theódór Magnússon <teddi@menn.is>
2017-09-28 03:00:39 +0000

    some foundational structures

--- script/main.js
+++ script/main.js
@@ -2,10 +2,31 @@ $(document).ready( () => {
 	$('#editor').focus();
 
 	$("#editor").on("keydown keyup", function(e){
+		let code = $('#editor').html();
 		if(e.key === "Tab"){
 			e.preventDefault();
-			$("#editor").text($("#editor").text() + ' ');
+			// const caret = $("#editor").caret();
+			// code = code.substring(0, caret) + `&nbsp &nbsp` + code.substring(caret, code.length);
+			// $("#editor").html(code);
+			// $("#editor").caret(caret + 6);
 		}
-		console.log(e.key);
+		parseCSS();
 	});
 });
+
+function parseCSS() {
+	let code = $("#editor").text().replace(/\s/g, '');
+	let styles = code.match(/\{(.*?)\}/g);
+	console.log(styles);
+	$('#testButton').removeAttr('style');
+
+	styles && styles.forEach((component, index) => {
+		styles[index] = component.slice(1,component.length - 1);
+		let styleset = styles[index].split(';');
+		styleset.forEach((style, index) => {
+			let currentStyle = style.split(':');
+			// console.log(currentStyle);
+			$('#testButton').css(currentStyle[0], currentStyle[1]);
+		});
+	});
+}
